734352b4c9a6fff4fa9570437aca8c365e3efd3a
Alexis Orozco <ch220111260@chapala.tecmm.edu.mx>
2025-04-01 21:51:24 -0600

    Add files via upload

---
+++ proyecto-web-main/vista_lista/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+	document.title = "Lista de Convenios";
+	
+	// DataTable initialization
+	var table = $("#example").DataTable({
+		dom: '<"dt-top"<"dt-length"l><"dt-buttons"B><"dt-search"f>>rt<"dt-bottom"ip>',
+		responsive: true,
+		language: {
+			url: '//cdn.datatables.net/plug-ins/1.13.7/i18n/es-ES.json'
+		},
+		buttons: [
+			'copy',
+			'csv',
+			'excel',
+			'pdf',
+			'print'
+		]
+	});
+});
